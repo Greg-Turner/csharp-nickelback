3cc628d6d22974584e6b812bc9582bbab74a3c6c
Greg Turner <31353577+Greg-Turner@users.noreply.github.com>
2018-01-18 07:43:28 -0600

    Completed a C-Sharp exercise using lists, hashsets, and tuples.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nickelback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string, string)> goodSongs = new List<(string, string)>();
+            HashSet<(string, string)> allSongs = new HashSet<(string, string)>();
+
+            allSongs.Add(("Nickelback", "Gotta Be Somebody"));
+            allSongs.Add(("Nickelback", "RockStar"));
+            allSongs.Add(("Nickelback", "If Today Was Your Last Day"));
+            allSongs.Add(("Imagine Dragons", "Thunder"));
+            allSongs.Add(("Beastie Boys", "Fight For Your Right"));
+            allSongs.Add(("Barry Manilow", "Mandy"));
+            allSongs.Add(("Def Leppard", "Pour Some Sugar On Me"));
+
+            foreach ((string, string) item in allSongs)
+            {
+                if (!item.Item1.Contains("Nickelback"))
+                {
+                    goodSongs.Add(item);
+                }
+            }
+            foreach ((string, string) song in goodSongs)
+            {
+                Console.WriteLine($"{song.Item1} has a good song titled {song.Item2}.");
+            }
+        }
+    }
+}
